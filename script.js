a7b08e28fe72e82f9c93885221df935a653ade20
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-09 14:49:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (Map, MapView, FeatureLayer) {
         var map = new Map({
           basemap: "topo-vector"
         });
@@ -6,7 +6,19 @@ require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
+          zoom: 10,
+          center: [-90.4423392383084,38.62829009276375] // longitude, latitude
         });
+        
+        var zip_codes = new FeatureLayer({
+                url: "https://services2.arcgis.com/w657bnjzrjguNyOy/arcgis/rest/services/ZipCodes_StLouisCounty/FeatureServer/0"
+        });
+
+        map.add(zip_codes);
+
+        var municipalities = new FeatureLayer({
+                url: "https://maps.stlouisco.com/arcgis/rest/services/OpenData/OpenData/FeatureServer/6"
+        });
+
+        map.add(municipalities);
       });
